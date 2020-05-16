2d92a71539b461b328ba4c2143cbe399c2d32ac2
unknown <weryogesh54@gmail.com>
2020-05-16 11:04:01 +0530

    Updates

---
+++ precache-manifest.e0be2eb49d9d88817bb4785cfa8da610.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d399cab4ef55eac04b10ca1e0bdeea",
+    "url": "/movies-series-info/index.html"
+  },
+  {
+    "revision": "8d534f853d3d63d17ac3",
+    "url": "/movies-series-info/static/css/main.0d7c4b80.chunk.css"
+  },
+  {
+    "revision": "ee73d46be38a192c0bfa",
+    "url": "/movies-series-info/static/js/2.867816f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-series-info/static/js/2.867816f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d534f853d3d63d17ac3",
+    "url": "/movies-series-info/static/js/main.3cff3253.chunk.js"
+  },
+  {
+    "revision": "61be121c633bfbcbbff7",
+    "url": "/movies-series-info/static/js/runtime-main.27e953e3.js"
+  },
+  {
+    "revision": "0efe72768e63eacecbbc255db5506415",
+    "url": "/movies-series-info/static/media/Spinner.0efe7276.gif"
+  }
+]);
