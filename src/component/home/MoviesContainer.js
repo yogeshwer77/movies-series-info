000fc43101dd65c765d10eae8ca8670c289bdbc3
unknown <weryogesh54@gmail.com>
2020-05-17 08:34:15 +0530

    first commit

--- src/component/home/MoviesContainer.js
+++ src/component/home/MoviesContainer.js
@@ -7,9 +7,8 @@ export class MoviesContainer extends Component {
     render() {
         const {movies} = this.props
         let content = ''
-
-        // content = movies.length > 0 ? movies.map((movie, index) => <MovieCard key={index} movie={movie} />) : null
         content = movies.Response == "True" ? movies.Search.map((movie, index) => <MovieCard key={index} movie={movie} />) : null
+        // content = movies.length > 0 ? movies.map((movie, index) => <MovieCard key={index} movie={movie} />) : null
         return (
             <div className='row'>
                 {content}
